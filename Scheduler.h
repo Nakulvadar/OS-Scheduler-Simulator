f9f368d77b4e0fc856c9c82383cefd43e9f58e36
Nakulvadar <nakulvadar@gmail.com>
2026-07-28 10:56:43 +0530

    Refactor Scheduler class and add include guards

--- Scheduler.h
+++ Scheduler.h
@@ -1,44 +1,23 @@
-#include "Process.h"
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
 #include <vector>
-#include<iostream>
+#include "Process.h"
 
 using namespace std;
 
-class Scheduler{
-
+class Scheduler
+{
 protected:
     vector<Process> processes;
+
 public:
-
-    void addProcess(const Process &process);
-
-    void displayProcesses() const;
-
     virtual void schedule() = 0;
 
+    void addProcess(Process p);
+    void displayProcesses();
+
+    virtual ~Scheduler() {}
 };
 
-
-
-class FCFS : public Scheduler
-{
-public:
-    void schedule() override;
-};
-
-class SJF : public Scheduler
-{
-public:
-    void schedule() override;
-};
-
-class RoundRobin : public Scheduler
-{
-    int quantum ;
-public:
-    RoundRobin(int q) : quantum(q)
-    {
-    }
-
-    void schedule() override;
-};
+#endif
